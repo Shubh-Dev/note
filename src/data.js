53775b937b911c581a8754d4a711091bc4795f82
Shubh <shubh.scb@gmail.com>
2023-03-07 22:55:10 +0530

    EDITOR SIDEBAR COMPONENTS

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: 1,
+    body: `### This just in! Markdown is an *awesome* way to write **basic HTML** by typing manually. 
+        
+        It can be used for:
+         1. README files
+         1. Slack and Discord messages 
+         1. **So much more** 
+         
+         Here's a division`,
+  },
+  { id: 2, body: 'a' },
+  { id: 3, body: 'b' },
+  { id: 4, body: 'c' },
+  { id: 5, body: 'd' },
+  { id: 6, body: 'e' },
+];
